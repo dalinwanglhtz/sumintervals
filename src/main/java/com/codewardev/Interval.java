02e3ff717ed9da56543746f86b8d8e796873e798
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-18 20:47:27 +1100

    started

---
+++ src/main/java/com/codewardev/Interval.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/java
+
+public class Interval {
+
+	public static int sumIntervals(int[][] intervals) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
