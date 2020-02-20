3789c81d1992dbe6857df3f52ebaad3cbc3f85a1
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-20 22:24:54 +1100

    first working cut

--- src/main/java/com/codewardev/Interval.java
+++ src/main/java/com/codewardev/Interval.java
@@ -1,7 +1,9 @@
 package com.codewardev;
 
+//https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/java
 
-// https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/java
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Interval {
 
@@ -9,31 +11,53 @@ public class Interval {
 		
 		if(intervals == null || intervals.length == 0) return 0;
 		
-		// Need to sort it first
-		
 		int currMin;
 		int currMax;
 		int total = 0;
-		int[][] pIntervals = intervals;
+		int[][] pIntervals = new int[intervals.length][2];
+		for(int a=0; a<intervals.length; a++) {
+			pIntervals[a][0] = intervals[a][0];
+			pIntervals[a][1] = intervals[a][1];
+		}
+		
+		// Need to sort it first
+		Arrays.sort(pIntervals, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				return o1[0] - o2[0];
+			}
+			
+		});
+		
+//		for(int i=0; i<pIntervals.length; i++) {
+//			System.out.println("first: "+pIntervals[i][0]+" second: "+pIntervals[i][1]);
+//		}
+		
+		
 		for(int i=0; i<pIntervals.length; i++) {
 			int[] currInterval = pIntervals[i];
 			currMin = currInterval[0];
 			currMax = currInterval[1];
-			System.out.println("currMin: "+currMin+" currMax: "+currMax+ " total: "+total);
+		//	System.out.println("currMin: "+currMin+" currMax: "+currMax+ " total: "+total);
 			if(currMax - currMin == 0) continue;
 			
 			if(i+1 < pIntervals.length) {
 				for(int j=i+1; j<pIntervals.length; j++) {
+					System.out.print("first: "+pIntervals[j][0]+" second: "+pIntervals[j][1]);
 					if(pIntervals[j][0] >= currMin && pIntervals[j][0] <= currMax
 							|| pIntervals[j][1] <= currMax && pIntervals[j][1] >= currMin) {
+						System.out.println(" is overlap with "+currMin+" or "+currMax);
 						currMin = Math.min(currMin, pIntervals[j][0]);
 						currMax = Math.max(currMax, pIntervals[j][1]);
 						pIntervals[j][0] = 0;
 						pIntervals[j][1] = 0;
-					}
+					} else
+						System.out.println();
 				}
 			}
-			System.out.println("currMin: "+currMin+" currMax: "+currMax+" now!");
+		//	System.out.println("currMin: "+currMin+" currMax: "+currMax+" now!");
 			total += currMax - currMin;
 		}
 
--- src/test/java/com/codewardev/IntervalTest.java
+++ src/test/java/com/codewardev/IntervalTest.java
@@ -8,23 +8,23 @@ public class IntervalTest {
 
   @Test
 	public void shouldHandleNullOrEmptyIntervals() {
-//		assertEquals(0, Interval.sumIntervals(null));
-//		assertEquals(0, Interval.sumIntervals(new int[][]{}));
-//		assertEquals(0, Interval.sumIntervals(new int[][]{{4, 4}, {6, 6}, {8, 8}}));
+		assertEquals(0, Interval.sumIntervals(null));
+		assertEquals(0, Interval.sumIntervals(new int[][]{}));
+		assertEquals(0, Interval.sumIntervals(new int[][]{{4, 4}, {6, 6}, {8, 8}}));
 	}
 
 	@Test
 	public void shouldAddDisjoinedIntervals() {
-//		assertEquals(9, Interval.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
-//		assertEquals(11, Interval.sumIntervals(new int[][]{{4, 8}, {9, 10}, {15, 21}}));
-//		assertEquals(7, Interval.sumIntervals(new int[][]{{-1, 4}, {-5, -3}}));
-//		assertEquals(78, Interval.sumIntervals(new int[][]{{-245, -218}, {-194, -179}, {-155, -119}}));
+		assertEquals(9, Interval.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
+		assertEquals(11, Interval.sumIntervals(new int[][]{{4, 8}, {9, 10}, {15, 21}}));
+		assertEquals(7, Interval.sumIntervals(new int[][]{{-1, 4}, {-5, -3}}));
+		assertEquals(78, Interval.sumIntervals(new int[][]{{-245, -218}, {-194, -179}, {-155, -119}}));
 	}
 
 	@Test
 	public void shouldAddAdjacentIntervals() {
-//		assertEquals(54, Interval.sumIntervals(new int[][]{{1, 2}, {2, 6}, {6, 55}}));
-//		assertEquals(23, Interval.sumIntervals(new int[][]{{-2, -1}, {-1, 0}, {0, 21}}));
+		assertEquals(54, Interval.sumIntervals(new int[][]{{1, 2}, {2, 6}, {6, 55}}));
+		assertEquals(23, Interval.sumIntervals(new int[][]{{-2, -1}, {-1, 0}, {0, 21}}));
 	}
 
 	@Test
@@ -36,9 +36,9 @@ public class IntervalTest {
 
 	@Test
 	public void shouldHandleMixedIntervals() {
-//		assertEquals(13, Interval.sumIntervals(new int[][]{{2, 5}, {-1, 2}, {-40, -35}, {6, 8}}));
-//		assertEquals(1234, Interval.sumIntervals(new int[][]{{-7, 8}, {-2, 10}, {5, 15}, {2000, 3150}, {-5400, -5338}}));
-//		assertEquals(158, Interval.sumIntervals(new int[][]{{-101, 24}, {-35, 27}, {27, 53}, {-105, 20}, {-36, 26},}));
+		assertEquals(13, Interval.sumIntervals(new int[][]{{2, 5}, {-1, 2}, {-40, -35}, {6, 8}}));
+		assertEquals(1234, Interval.sumIntervals(new int[][]{{-7, 8}, {-2, 10}, {5, 15}, {2000, 3150}, {-5400, -5338}}));
+		assertEquals(158, Interval.sumIntervals(new int[][]{{-101, 24}, {-35, 27}, {27, 53}, {-105, 20}, {-36, 26},}));
 	}
   
 }
